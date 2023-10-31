2eaae0b3cbd36d6920c790c33bc424db32b6909c
Dimitry Lyomin <leminds@gmail.com>
2023-10-31 12:44:39 +0300

    Add prettier

--- front/pages/api-playground.tsx
+++ front/pages/api-playground.tsx
@@ -1,29 +1,29 @@
-import createApolloClient from '@/apollo.client';
-import { Query } from '@/generated-types';
-import { gql } from '@apollo/client';
+import createApolloClient from "@/apollo.client"
+import { Query } from "@/generated-types"
+import { gql } from "@apollo/client"
 
 export default function ApiPlayground() {
-
   const testHandle = async () => {
-    const client = createApolloClient();
+    const client = createApolloClient()
     const { data } = await client.query<Query>({
       query: gql`
         query Users {
           user(id: "1") {
-            id,
-            firstName,
-            lastName,
+            id
+            firstName
+            lastName
             creationDate
           }
         }
       `,
-    });
+    })
 
-    console.log('FE - user', data.user);
-    
+    console.log("FE - user", data.user)
   }
 
-  return <>
-    <button onClick={testHandle}>Test api</button>
-  </>
+  return (
+    <>
+      <button onClick={testHandle}>Test api</button>
+    </>
+  )
 }
