dba56d803edac651e91d6cc4e0ce7011905df1b2
Dimitry Lyomin <leminds@gmail.com>
2023-11-16 21:39:33 +0300

    Minor updates

--- front/components/index/TimerClock.tsx
+++ front/components/index/TimerClock.tsx
@@ -21,6 +21,7 @@ export const TimerClock: FC<Props> = ({ begin }) => {
 
       setCurrInterval(intervalId)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [begin])
 
   if (!begin) return null
